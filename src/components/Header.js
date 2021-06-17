ee12ee0790ec2949d398fb878f4390b9b57db14d
mayur2139 <mayurnagre22@gmail.com>
2021-06-17 19:27:05 +0530

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = (props) => {
   return (
     <div>
       <nav className="navbar navbar-inverse navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">SkillApp</a>
+        <a className="navbar-brand" href="#">Post-Album-Task Management System</a>
 
         <div className="navbar-nav">
           <span className="nav-item nav-link">
